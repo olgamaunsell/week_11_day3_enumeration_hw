96061843fbebef47e5c6bfbfa51d6d19b91ede74
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-02-14 18:09:59 +0000

    7 tests passing

--- array_tasks.js
+++ array_tasks.js
@@ -1,32 +1,60 @@
 const arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+    return arr1.concat(arr2);
+	},
 
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+    arr.splice(index, 0, itemToAdd);
+    return arr;
+	},
 
-	// insertAt: function (arr, itemToAdd, index) {
+	square: function (arr) {
+    const result = arr.map ( function(number) {
+      return number * number;
+    })
+    return result;
+	},
 
-	// },
+	sum: function (arr) {
+    const total = arr.reduce(function(runningTotal, number){
+    return runningTotal + number;
+    }, 0)
 
-	// square: function (arr) {
+    return total;
 
-	// },
+	},
 
-	// sum: function (arr) {
+	removeAndClone: function (arr, valueToRemove) {
+    const filteredArray = arr.filter(function(number){
+      return number !== valueToRemove;
+    });
+    return filteredArray;
+	},
 
-	// },
 
-	// removeAndClone: function (arr, valueToRemove) {
 
-	// },
+	findIndexesOf: function (arr, itemToFind) {
+    const indexArray = [];
 
-	// findIndexesOf: function (arr, itemToFind) {
+    arr.forEach(function(number, index){
+      if(number === itemToFind){
+        indexArray.push(index);
+      }
+    });
+    return indexArray;
+	},
 
-	// },
+	sumOfAllEvenNumbersSquared: function (arr) {
 
-	// sumOfAllEvenNumbersSquared: function (arr) {
+  const evenNumbers = arr.filter(function(number){
+    return number % 2 === 0;
+  });
 
-	// }
+  const squaredNumbers = arrayTasks.square(evenNumbers);
+  const sum = arrayTasks.sum(squaredNumbers);
+  return sum;
+	}
 
 	// ----------- EXTENSION ------------
 
--- tests.js
+++ tests.js
@@ -3,39 +3,39 @@ const arrayTasks = require('./array_tasks.js')
 
 describe('Array tasks', function () {
 
-	xit('should concatenate two arrays, returning a new array', function () {
+	it('should concatenate two arrays, returning a new array', function () {
 		const arr1 = [1, 2, 3]
 		const arr2 = [4, 5, 6]
 		const expectation = [1, 2, 3, 4, 5, 6]
 		assert.deepStrictEqual(arrayTasks.concat(arr1, arr2), expectation)
 	})
 
-	xit('should insert an item in an array at any index position', function () {
+	it('should insert an item in an array at any index position', function () {
 		const arr = [1, 2, 4]
 		assert.deepStrictEqual(arrayTasks.insertAt(arr, 3, 2), [1, 2, 3, 4])
 	})
 
-	xit('should square all values in an array, returning a new array', function () {
+	it('should square all values in an array, returning a new array', function () {
 		const arr = [1, 2, 3, 4, 5]
 		assert.deepStrictEqual(arrayTasks.square(arr), [1, 4, 9, 16, 25])
 	})
 
-	xit('should calculate the sum of all values in an array', function () {
+	it('should calculate the sum of all values in an array', function () {
 		const arr = [1, 2, 3, 4, 5]
 		assert.strictEqual(arrayTasks.sum(arr), 15)
 	})
 
-	xit('should remove all instances of a value from an array, returning a new array', function () {
+	it('should remove all instances of a value from an array, returning a new array', function () {
 		const arr = [1, 2, 3, 1, 4, 5, 1]
 		assert.deepStrictEqual(arrayTasks.removeAndClone(arr, 1), [2, 3, 4, 5])
 	})
 
-	xit('should find all occurences of a value, returning an array of index positions', function () {
+	it('should find all occurences of a value, returning an array of index positions', function () {
 		const arr = [1, 2, 1, 3, 4, 1, 5]
 		assert.deepStrictEqual(arrayTasks.findIndexesOf(arr, 1), [0, 2, 5])
 	})
 
-	xit('should calculate the sum of all of even numbers in an array squared', function () {
+	it('should calculate the sum of all of even numbers in an array squared', function () {
 		const arr = [1, 2, 3, 4, 5]
 		assert.strictEqual(arrayTasks.sumOfAllEvenNumbersSquared(arr), 20)
 	})
